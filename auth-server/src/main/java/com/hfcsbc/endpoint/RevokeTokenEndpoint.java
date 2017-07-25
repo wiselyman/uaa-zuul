df2b0b2bc96990d19ba0a0d0b3cc145e2da5b93e
wangyunfei <wiselyman2008@gmail.com>
2017-07-25 11:53:37 +0800

    add logout

---
+++ auth-server/src/main/java/com/hfcsbc/endpoint/RevokeTokenEndpoint.java
@@ -0,0 +1,31 @@
+package com.hfcsbc.endpoint;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpoint;
+import org.springframework.security.oauth2.provider.token.ConsumerTokenServices;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by ZJ on 2017/7/24.
+ */
+@FrameworkEndpoint
+public class RevokeTokenEndpoint {
+
+    @Autowired
+    @Qualifier("consumerTokenServices")
+    ConsumerTokenServices consumerTokenServices;
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/oauth/token")
+    @ResponseBody
+    public String revokeToken(String access_token) {
+        if (consumerTokenServices.revokeToken(access_token)){
+            return "注销成功";
+        }else{
+            return "注销失败";
+        }
+    }
+}

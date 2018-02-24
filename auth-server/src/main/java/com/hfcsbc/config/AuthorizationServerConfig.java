28ffc5dd5f0b791c9a6a5610a875b495d318fd0c
非晓兵 <fxb@amoebo.com>
2018-02-24 16:38:30 +0800

    修复编译报错,注入UserDtailService;

--- auth-server/src/main/java/com/hfcsbc/config/AuthorizationServerConfig.java
+++ auth-server/src/main/java/com/hfcsbc/config/AuthorizationServerConfig.java
@@ -1,34 +1,17 @@
 package com.hfcsbc.config;
 
-import com.hfcsbc.security.DomainUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.code.AuthorizationCodeServices;
-import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
-import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
-
-import javax.jws.soap.SOAPBinding;
-import javax.sql.DataSource;
-import java.security.KeyPair;
 
 /**
  * Created by wangyunfei on 2017/6/9.
@@ -40,6 +23,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
     private AuthenticationManager authenticationManager;
     @Autowired
     private RedisConnectionFactory connectionFactory;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
 
     @Bean
@@ -70,7 +55,7 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 .scopes("xx")
                 .secret("android")
                 .authorizedGrantTypes("password", "authorization_code", "refresh_token")
-            .and()
+                .and()
                 .withClient("webapp")
                 .scopes("xx")
                 .authorizedGrantTypes("implicit");
